37c2d85db1f853dc0cfd5a8fe09ef152a14915d6
jaeyunim <ijy00120877@gmail.com>
2022-02-13 19:04:04 +0900

    edit header, +BADGE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const search = document.querySelector('.search');
 const searchInput = search.querySelector('input');
+const badgeEl =document.querySelector('.badges')
 
 function handleSearch() {
     searchInput.focus();
@@ -16,3 +17,18 @@ function handleBlur() {
 search.addEventListener("click", handleSearch);
 searchInput.addEventListener("focus", handleFoucs);
 searchInput.addEventListener("blur", handleBlur);
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        gsap.to(badgeEl, 0.6, {
+            opacity: 0,
+            display: "none"
+        })
+    } else {
+        gsap.to(badgeEl, 0.6, {
+            opacity: 1,
+            display: "block"
+        })
+    }
+}, 300));
